1b5a78596bf579dc87c6f5dca598890d67d5052d
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-03-20 13:48:12 +0530

    Working on Carousel

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 //#----------------------------------------- <<< Toggle Hamburger Menu --> JQuery >>> ----------------------------------------------//
 
-$(document).ready(function() {
-    $nav = $('.nav');
-    $toggleCollapse = $('.toggle_collapse');
-    
-    // Click event on toggle menu
-    $toggleCollapse.click(function(){
-        $nav.toggleClass('collapse');
-        
-    })
+$(document).ready(function () {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle_collapse");
+
+  // Click event on toggle menu
+  $toggleCollapse.click(function () {
+    $nav.toggleClass("collapse");
+  });
 });
 
 //#----------------------------------------- >>> Toggle Hamburger Menu --> JQuery <<< ----------------------------------------------//
+
+// Initializing Owl-Carousel
+$(".owl-carousel").owlCarousel();
