85b6a68b496d245171f2f5f08937bc758d1e3f65
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-03-21 19:56:25 +0530

    Added responsiveness and all.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 //#----------------------------------------- <<< Toggle Hamburger Menu --> JQuery >>> ----------------------------------------------//
 
+const responsive = {
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  },
+};
+
 $(document).ready(function () {
   $nav = $(".nav");
   $toggleCollapse = $(".toggle_collapse");
@@ -8,19 +23,30 @@ $(document).ready(function () {
   $toggleCollapse.click(function () {
     $nav.toggleClass("collapse");
   });
-});
 
-//#----------------------------------------- >>> Toggle Hamburger Menu --> JQuery <<< ----------------------------------------------//
+  //#----------------------------------------- >>> Toggle Hamburger Menu --> JQuery <<< ----------------------------------------------//
 
-// Initializing Owl-Carousel
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  dots: false,
-  nav: true,
-  navText: [
-    $(".owl-navigation .owl-nav-prev"),
-    $(".owl-navigation .owl-nav-next"),
-  ],
+  // Initializing Owl-Carousel
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
+    responsive: responsive,
+  });
+
+  // Click to scroll up to the top
+  $(".move-up span").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1300
+    );
+  });
 });
